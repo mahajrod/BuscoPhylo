692135c8c7fcc1fd8e10536574c2ccc38230bb26
tomarovsky <andrey.tomarovsky@gmail.com>
2021-09-18 08:40:26 +0300

    fix

--- workflow/scripts/concat_fasta.py
+++ workflow/scripts/concat_fasta.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
 
-from Bio import SeqIO
 from collections import defaultdict
+from Bio import SeqIO
+from sys import stdin
+import argparse
 
 
 def main():
     sequence_map = defaultdict(str)
 
-    for sequence in SeqIO.parse('all.fasta', "fasta"):
+    for sequence in SeqIO.parse(args.input, "fasta"):
         sequence_map[sequence.name] += str(sequence.seq)
 
-    for key in sorted(sequence_map.iterkeys()):
-        print('>' + key)
-        print(sequence_map[key])
+    outfile = open(args.output, "w")
+    for key in sequence_map.keys():
+        outfile.write('>' + key + '\n')
+        outfile.write(sequence_map[key] + '\n')
+    outfile.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="script for concatenate FASTA files into one big FASTA file "
                                                  "by concatenating sequences with the same identifier")
     group_required = parser.add_argument_group('Required options')
-    group_required.add_argument('-i', '--input', type=str, help="FASTA files directory")
-    group_required.add_argument('-s', '--single_copy_files', type=str, nargs='+',
-                                help="list of single_copy_busco_sequences files")
+    group_required.add_argument('-i', '--input', type=str, default=stdin, help="input concat FASTA file "
+                                                                               "with the same headers or stdin")
     group_required.add_argument('-o', '--output', type=str, help="output FASTA file name")
     args = parser.parse_args()
     main()
