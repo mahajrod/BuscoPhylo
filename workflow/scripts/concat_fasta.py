7b49c6f6dcc78e8a7ba96a01d2ab8f27d04194d1
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-18 18:12:12 +0300

    stdin been removed

--- workflow/scripts/concat_fasta.py
+++ workflow/scripts/concat_fasta.py
@@ -8,12 +8,8 @@ import argparse
 
 def main():
     sequence_map = defaultdict(str)
-    if args.input is not stdin:
-        for i in args.input:
-            for sequence in SeqIO.parse(i, "fasta"):
-                sequence_map[sequence.name] += str(sequence.seq)
-    else:
-        for sequence in SeqIO.parse(args.input, "fasta"):
+    for i in args.input:
+        for sequence in SeqIO.parse(i, "fasta"):
             sequence_map[sequence.name] += str(sequence.seq)
 
     outfile = open(args.output, "w")
@@ -26,8 +22,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="script for concatenate FASTA files into one big FASTA file "
                                                  "by concatenating sequences with the same identifier")
     group_required = parser.add_argument_group('Required options')
-    group_required.add_argument('-i', '--input', type=str, default=stdin,
-                                nargs="+", help="input concat FASTA file with the same headers or stdin")
+    group_required.add_argument('-i', '--input', type=str, nargs="+",
+                                help="input list of concat FASTA files with the same headers")
     group_required.add_argument('-o', '--output', type=str, help="output FASTA file name")
     args = parser.parse_args()
     main()
